8f6ec04cdf255c5b2e705a31a0ef6431a080694b
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-02-19 21:09:22 +0000

    gggggggggggggggggggggg

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -43,25 +43,9 @@ const Navbar = () => {
         boxShadow={{ base: "xl", lg: "none" }}
         zIndex="2"
       >
-        <chakra.li
-          listStyleType="none"
-          px={{ lg: "8" }}
-          py={{ base: "3", lg: "0" }}
-        >
-          <NextLink href="/">
-            <a onClick={closeMenu}>Home</a>
-          </NextLink>
-        </chakra.li>
 
-        <chakra.li
-          listStyleType="none"
-          px={{ lg: "8" }}
-          py={{ base: "3", lg: "0" }}
-        >
-          <NextLink href="/blog">
-            <a onClick={closeMenu}>Blog</a>
-          </NextLink>
-        </chakra.li>
+
+
 
         {/* Add additional list items for Mint, Vaults, Earn, and Rewards */}
         <chakra.li
@@ -69,7 +53,7 @@ const Navbar = () => {
           px={{ lg: "8" }}
           py={{ base: "3", lg: "0" }}
         >
-          <NextLink href="/mint">
+          <NextLink href="/">
             <a onClick={closeMenu}>Mint</a>
           </NextLink>
         </chakra.li>
@@ -103,7 +87,15 @@ const Navbar = () => {
             <a onClick={closeMenu}>Rewards</a>
           </NextLink>
         </chakra.li>
-        {/* End of additional list items */}
+        <chakra.li
+          listStyleType="none"
+          px={{ lg: "8" }}
+          py={{ base: "3", lg: "0" }}
+        >
+          <NextLink href="/more">
+            <a onClick={closeMenu}>More</a>
+          </NextLink>
+        </chakra.li>
       </chakra.ul>
 
       <ColorModeToggle />
