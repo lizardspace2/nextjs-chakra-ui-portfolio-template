050e932fa29610e8ffbf4e6fc01190fbc319acd6
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-02-20 17:54:49 +0000

    hikjgtjhkgfhghgfhy

---
+++ src/pages/stake-prt.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Box,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import UnstakedBalance from './../components/stabilitypools/UnstakedBalance';
+import Pool from './../components/stabilitypools/Pool';
+import StakePRTComponent from '@/components/stakeprt/StakePRTComponent';
+
+const StakingPrtComponent = () => {
+  const bg = useColorModeValue('white', 'gray.700');
+  const borderColor = useColorModeValue('gray.200', 'gray.600');
+
+  return (
+    <Box borderWidth="1px" borderRadius="lg" borderColor={borderColor} p={5} bg={bg}>
+      <Text fontSize="2xl" fontWeight="bold" mb={5}>Staking Pools</Text>
+      <Box borderWidth="1px" borderRadius="lg" borderColor={borderColor} p={5} mb={5}>
+        <StakePRTComponent/>
+      </Box>
+      <UnstakedBalance
+        logoSrc="path-to-your-parrot-logo.png"
+        name="Parrot Pool"
+        apr={7}
+        token="prtSOL"
+        amount="88,179.27"
+        onWithdraw={() => {
+          // Handle withdraw logic
+        }}
+      />
+    </Box>
+  );
+};
+
+export default StakingPrtComponent;
